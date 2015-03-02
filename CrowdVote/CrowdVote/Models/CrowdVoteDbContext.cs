e9772374b80fa9d19b74b569f377f0a9a135b33a
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-02 15:17:52 +0100

    Created a Database context

---
+++ CrowdVote/CrowdVote/Models/CrowdVoteDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CrowdVote.Models
+{
+    public class CrowdVoteDbContext : DbContext
+    {
+        public CrowdVoteDbContext()
+            : base("CrowdVoteDbContext")
+        {
+        }
+
+
+    }
+}
