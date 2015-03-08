85a06ffa60c186df10caf16f7a4b3bf2f4cb0398
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-08 20:04:28 +0100

    changed routing to start with the subject controller

--- CrowdVote/CrowdVote/App_Start/RouteConfig.cs
+++ CrowdVote/CrowdVote/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CrowdVote
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Subject", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
