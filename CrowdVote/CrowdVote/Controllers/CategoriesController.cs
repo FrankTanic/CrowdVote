3ece00073fe64aaaaf2b9051c62d12acea2cb96b
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-08 20:06:28 +0100

    Created a Categories controller

---
+++ CrowdVote/CrowdVote/Controllers/CategoriesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrowdVote.Models;
+
+namespace CrowdVote.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private CrowdVoteDbContext _db = new CrowdVoteDbContext();
+
+        // GET: Home
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                HttpNotFound();
+            }
+
+            Subject subject = _db.Subjects.Find(id);
+
+            if(subject == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(subject);
+        }
+    }
+}
