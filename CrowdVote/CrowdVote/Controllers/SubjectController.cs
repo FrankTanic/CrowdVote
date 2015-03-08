dda8a36687297b3238e77183d530763fe18d21f0
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-08 20:05:19 +0100

    added a subject controller

---
+++ CrowdVote/CrowdVote/Controllers/SubjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrowdVote.Models;
+
+namespace CrowdVote.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private CrowdVoteDbContext _db = new CrowdVoteDbContext();
+
+        // GET: Subject
+        public ActionResult Index()
+        {
+            var subjects = _db.Subjects.ToList();
+
+            return View(subjects);
+
+        }
+    }
+}
