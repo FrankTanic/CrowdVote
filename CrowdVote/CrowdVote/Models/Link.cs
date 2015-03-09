4e28c16014d3d1205893127f6c9f82337ac751f2
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-09 14:58:34 +0100

    Created a model for the upvote links

---
+++ CrowdVote/CrowdVote/Models/Link.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrowdVote.Models
+{
+    public class Link
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Title of the page")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "You need to specify the URL, otherwise people can't visit the page you found")]
+        [Display(Name = "URL of the page")]
+        public string Url { get; set; }
+
+        public int VoteCount { get; set; }
+    }
+}
