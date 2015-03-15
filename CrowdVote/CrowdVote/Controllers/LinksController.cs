99f79e2bf4a8203c972687945b70f40acc0c35c1
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-15 15:59:03 +0100

    Created a create action for posting new links in the link controller

--- CrowdVote/CrowdVote/Controllers/LinksController.cs
+++ CrowdVote/CrowdVote/Controllers/LinksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CrowdVote.Models;
+using System.Net;
 
 namespace CrowdVote.Controllers
 {
@@ -11,22 +12,39 @@ namespace CrowdVote.Controllers
     {
         private CrowdVoteDbContext _db = new CrowdVoteDbContext();
 
-        public ActionResult Create()
+        public ActionResult Create(int? topicId)
         {
+            if (topicId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var topic = _db.Topics.Find(topicId);
+
+            if (topic == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Topic = topic.Title;
+
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(int? linkId, Link model)
+        public ActionResult Create(int? topicId, Link model)
         {
-            Topic topic = _db.Topics.Find(linkId);
+            Topic topic = _db.Topics.Find(topicId);
 
            
             if (ModelState.IsValid)
             {
                 topic.Links.Add(model);
 
-                return RedirectToAction("Details", "Topics", new { id = model.ID });
+                _db.Links.Add(model);
+                _db.SaveChanges();
+
+                return RedirectToAction("Details", "Topics", new { id = topicId });
             }
 
             return View(model);
