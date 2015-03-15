872844ec00351b3ba0bbb7f39a9962d623108ade
Frank van Driel <frank.vandriel@hotmail.com>
2015-03-15 16:00:52 +0100

    Created a new dbset for links in the CrowdVoteDbContext

--- CrowdVote/CrowdVote/Models/CrowdVoteDbContext.cs
+++ CrowdVote/CrowdVote/Models/CrowdVoteDbContext.cs
@@ -15,6 +15,6 @@ namespace CrowdVote.Models
 
         public DbSet<Category> Category { get; set; }
         public DbSet<Topic> Topics { get; set; }
-
+        public DbSet<Link> Links { get; set; }
     }
 }
